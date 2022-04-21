e8b2cd5a1833a577060383e2e1f8e6cc21003572
Vleezy <vladdromero@gmail.com>
2022-04-21 15:35:38 -0400

    Redux checkpoint

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,11 +11,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <React.StrictMode>
   
-    <BrowserRouter>
+    
     <Provider store={Store}>
+    <BrowserRouter>
     <App />
-    </Provider>
     </BrowserRouter>
+    </Provider>
+    
   </React.StrictMode>
 );
 
